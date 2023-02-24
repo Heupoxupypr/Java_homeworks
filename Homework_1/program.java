89e854c353b1f084967a8bf4ae51e163784fa55e
VVKozlov <vasyakus@gmail.com>
2023-02-24 10:37:10 +0300

    Add task for triangle numbers

--- Homework_1/program.java
+++ Homework_1/program.java
@@ -1,7 +1,27 @@
-
+// +Написать программу вычисления n-ого треугольного числа.
 
 package java_projects.Java_homeworks.Homework_1;
 
+import java.util.Scanner; //модуль для получения данных из терминала
+
 public class program {
-    
+    public static void main(String[] args) {
+
+        int n = GetNumber("Please enter number of triangle number: ");
+        
+        System.out.printf("Triangle number of %d is %d", n, GetTriangleNumber(n));       
+    }
+
+    public static int GetNumber(String message) {
+        System.out.println(message);
+        try (Scanner iScanner = new Scanner(System.in)) {
+            int n = Integer.parseInt(iScanner.nextLine());
+            return n;
+        }
+    }
+
+    public static int GetTriangleNumber(int number) {
+        number = (number * (number + 1)) / 2;
+        return number;
+    }
 }
