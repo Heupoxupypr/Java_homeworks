03c6afc140cc7714b5e180957745b88e50caa70b
VVKozlov <vasyakus@gmail.com>
2023-02-25 11:33:42 +0300

    Create Task_1.java

---
+++ Homework_2/Task_1/Task_1.java
@@ -0,0 +1,9 @@
+// Дана строка sql-запроса "select * from students where ". 
+// Сформируйте часть WHERE этого запроса, используя StringBuilder. 
+// Данные для фильтрации приведены ниже в виде json строки.
+
+package java_projects.Java_homeworks.Homework_2.Task_1;
+
+public class Task_1 {
+    
+}
