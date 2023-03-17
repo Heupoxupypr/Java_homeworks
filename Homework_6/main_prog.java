f370f18d22ed688f4b43f122e1ceb5ba9333a364
VVKozlov <vasyakus@gmail.com>
2023-03-17 09:35:52 +0300

    Add method setWall Add comments Add print map from homework 5

--- Homework_6/main_prog.java
+++ Homework_6/main_prog.java
@@ -1,3 +1,6 @@
+// -1 - стена, -2 - озеро, -3 - выход
+
+
 package java_projects.Java_homeworks.Homework_6;
 
 import java.util.ArrayList;
@@ -51,9 +54,11 @@ class Point2D {
   }
 }
 
+// Класс генерации карты
 class MapGenerator {
   int[][] map;
 
+  // конструктор класса
   public MapGenerator() {
     int[][] map = {
         { -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1 },
@@ -76,19 +81,28 @@ class MapGenerator {
     this.map = map;
   }
 
+  // Метод получения карты
   public int[][] getMap() {
     return map;
   }
 
-  public void setCat(Point2D pos) {
+  // Метод установки стены на карту
+  public void setWall(Point2D pos) {
+    map[pos.x][pos.y] = -1;
+  }
+
+  // Метод установки озера на карту
+  public void setLake(Point2D pos) {
     map[pos.x][pos.y] = -2;
   }
 
+  // Метод установки выхода на карту
   public void setExit(Point2D pos) {
     map[pos.x][pos.y] = -3;
   }
 }
 
+// Класс вывода карты на экран
 class MapPrinter {
 
   public MapPrinter() {
@@ -110,6 +124,7 @@ class MapPrinter {
     return sb.toString();
   }
 
+  // Метод для красивового вывода карты на экран
   public String mapColor(int[][] map) {
     StringBuilder sb = new StringBuilder();
 
@@ -117,13 +132,22 @@ class MapPrinter {
       for (int col = 0; col < map[row].length; col++) {
         switch (map[row][col]) {
           case 0:
-            sb.append("в–‘");
+            sb.append(" ");
             break;
           case -1:
-            sb.append("в–“");
+            if ((row == 0) || (row == map.length - 1)) {
+              sb.append("-");
+            } else if ((col == 0) || (col == map[row].length - 1)) {
+              sb.append("|");
+            } else if ((map[row][col - 1] == -1) || (map[row][col + 1] == -1)) {
+              sb.append("-");
+            } else if ((map[row - 1][col] == -1) || (map[row + 1][col] == -1)) {
+              sb.append("|");
+            } else
+              sb.append("=");
             break;
           case -2:
-            sb.append("Рљ");
+            sb.append("O");
             break;
           case -3:
             sb.append("E");
@@ -141,33 +165,48 @@ class MapPrinter {
   }
 }
 
+// Класс подсчета шагов по волновому алгоритму
 class WaveAlgorithm {
   int[][] map;
 
+  // При объявлении экземпляра класса, он должен на вход получать карту
   public WaveAlgorithm(int[][] map) {
     this.map = map;
   }
 
   public void Colorize(Point2D startPoint) {
+    // Создаем очередь для записи шагов волнового алгоритма
     Queue<Point2D> queue = new LinkedList<Point2D>();
+    // Помещаем стартовую точку в очередь
     queue.add(startPoint);
+    // устанавливаем значение шака в стартовой точке 1
     map[startPoint.x][startPoint.y] = 1;
 
+    // цикл опустошения очереди
     while (queue.size() != 0) {
+      // извлекаем из очереди следующий элемент
       Point2D p = queue.remove();
 
+      // если поле левее текущего не равно 0
+      // помещаем его в очередь и записываем значение шага в ячейку увеличенное на 1
       if (map[p.x - 1][p.y] == 0) {
         queue.add(new Point2D(p.x - 1, p.y));
         map[p.x - 1][p.y] = map[p.x][p.y] + 1;
       }
+      // если поле выше текущего не равно 0
+      // помещаем его в очередь и записываем значение шага в ячейку увеличенное на 1
       if (map[p.x][p.y - 1] == 0) {
         queue.add(new Point2D(p.x, p.y - 1));
         map[p.x][p.y - 1] = map[p.x][p.y] + 1;
       }
+      // если поле правее текущего не равно 0
+      // помещаем его в очередь и записываем значение шага в ячейку увеличенное на 1
       if (map[p.x + 1][p.y] == 0) {
         queue.add(new Point2D(p.x + 1, p.y));
         map[p.x + 1][p.y] = map[p.x][p.y] + 1;
       }
+      // если поле ниже текущего не равно 0
+      // помещаем его в очередь и записываем значение шага в ячейку увеличенное на 1
       if (map[p.x][p.y + 1] == 0) {
         queue.add(new Point2D(p.x, p.y + 1));
         map[p.x][p.y + 1] = map[p.x][p.y] + 1;
@@ -175,6 +214,7 @@ class WaveAlgorithm {
     }
   }
 
+  // метод поиска маршрута до переданной точки выхода
   public ArrayList<Point2D> getRoad(Point2D exit) {
     ArrayList<Point2D> road = new ArrayList<>();
     ///
