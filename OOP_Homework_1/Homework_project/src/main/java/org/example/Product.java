6704b5f63c13d29573684bf121f5561d8240955a
VVKozlov <vasyakus@gmail.com>
2023-03-24 06:51:06 +0300

    Correct Product constructors

--- OOP_Homework_1/Homework_project/src/main/java/org/example/Product.java
+++ OOP_Homework_1/Homework_project/src/main/java/org/example/Product.java
@@ -7,16 +7,35 @@ package org.example;
  * rating
  */
 public class Product {
-    String name;
-    double price;
-    double rating;
+    private String name;
+    private double price;
+    private double rating;
 
+
+    /**
+     * Full constructor for Product
+     *
+     * @param name   Name of product
+     * @param price  Price of product
+     * @param rating Raiting of product
+     */
     public Product(String name, double price, double rating) {
         this.name = name;
         this.price = price;
         this.rating = rating;
     }
 
-    public Product() {
+    public Product(String name) {
+        this(name, 0);
     }
+
+    public Product(String name, double price) {
+        this(name, price, 0);
+    }
+
+    public Product() {
+        this("");
+    }
+
+
 }
