fadc841a0d3029974f58c85f1d6e76dac0caf95a
VVKozlov <vasyakus@gmail.com>
2023-05-15 05:55:22 +0300

    Add code to class Category

--- Java_control_programm/src/Model/Category.java
+++ Java_control_programm/src/Model/Category.java
@@ -1,4 +1,45 @@
 package Model;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+
 public class Category {
+    private int categoryId;
+    private String categoryName;
+    private List<Toy> categoryToys;
+    private static int defaultIndex;
+    private static String[] defaultNames = {"Bears", "Rabbits", "Cars",
+            "TrainS", "Cranes", "Cubes",
+            "Pencils", "Markers"};
+    private static ArrayList<String> defaultCategoryNames;
+    private static ArrayList<String> Names;
+    protected static Random rnd;
+
+    static {
+        defaultIndex = 1;
+        defaultCategoryNames = new ArrayList<String>(Arrays.asList(defaultNames));
+        Names = new ArrayList<String>();
+        rnd = new Random();
+    }
+
+    public Category(String categoryName, List<Toy> categoryToys) {
+        if (Category.Names.indexOf(categoryName) != -1){
+            this.categoryName = String.format("CategoryDefaultName_%d", defaultIndex);
+        }
+        else {
+            this.categoryName = categoryName;
+        }
+        this.categoryToys.addAll(categoryToys);
+        this.categoryId = defaultIndex++;
+    }
+
+    public Category(String categoryName) {
+        this(categoryName,new ArrayList<Toy>());
+    }
+
+    public Category() {
+        this(defaultCategoryNames.get(rnd.nextInt(defaultCategoryNames.size())));
+    }
 }
