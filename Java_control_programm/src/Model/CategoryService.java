10751fe541956325d65fec8eabc29bfeccf27074
VVKozlov <vasyakus@gmail.com>
2023-05-16 07:51:49 +0300

    Add functions createFreeCategory and createCategory to class CategoryService

--- Java_control_programm/src/Model/CategoryService.java
+++ Java_control_programm/src/Model/CategoryService.java
@@ -1,4 +1,12 @@
 package Model;
 
+import java.util.List;
+
 public class CategoryService {
+    public Category createFreeCategory() {
+        return new Category();
+    };
+    public Category createCategory(String categoryName, List<Toy> toys) {
+        return new Category(categoryName, toys);
+    }
 }
