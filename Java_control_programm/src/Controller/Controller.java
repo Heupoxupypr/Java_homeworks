13ef877c78603186df53cfabc00666f1b54a627a
VVKozlov <vasyakus@gmail.com>
2023-05-17 10:30:32 +0300

    Create method start() in class Controller for testing program

--- Java_control_programm/src/Controller/Controller.java
+++ Java_control_programm/src/Controller/Controller.java
@@ -1,4 +1,24 @@
 package Controller;
 
+import Model.*;
+import View.View;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Controller {
+    public void start() {
+        ThingService thingService = new ThingService();
+        CategoryService categoryService = new CategoryService();
+        int categoryCount = 10;
+        int toysCount = 50;
+        List<Category> categoryList = new ArrayList<>();
+        List<Toy> toyList = new ArrayList<>();
+        for (int i = 0; i < toysCount; i++) {
+            toyList.add(thingService.createToy(new Toy()));
+        }
+
+        View view = new View();
+        System.out.println(view.toysView(toyList));
+    }
 }
